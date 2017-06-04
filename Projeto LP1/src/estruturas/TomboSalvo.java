6e56b9b15d600c184b8d28dccddd224fac06ffc8
drayt <drayt@DESKTOP-9762P9P>
2017-06-04 12:59:00 -0300

    1.7.7 - Comentários Revisados nas Estruturas menos ManipulaçãoDeImagem

--- Projeto LP1/src/estruturas/TomboSalvo.java
+++ Projeto LP1/src/estruturas/TomboSalvo.java
@@ -3,17 +3,18 @@ package estruturas;
 import java.util.LinkedList;
 
 public class TomboSalvo {
-    //Atributos
+   //Atributos:
     private static Persistencia p = new Persistencia();
     private LinkedList<Arte> cad = new LinkedList<>();
     private int tombo;
     private static int tomboAux = 0;
     
-    /** Método pega o atual número de tombo adquirido
-     *    Descrição:
-     *     O Metodo lê a coleção de objetos salvos no arquivo e retorna o maior
-     *     número de tombo salvo
-     */
+   //Métodos:
+     /** Método que Pega o Número de Tombo Adquirido
+      *    Descrição:
+      *      O método lê a coleção de objetos salvos no arquivo e retorna o maior
+      *      número de tombo salvo.
+      */
     public int getTomboSalvo() {
         p.setupLer();
         p.readRecords();
